cd921004033ae61cf553f4f698d6b64c9dfe6bc3
Shimelis <shimelisgetahun07@gmail.com>
2025-08-07 06:16:42 -0700

    forntend input form added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState} from 'react';
+
 
 function Home() {
+
+  const [input, setInput] = useState({});
+  
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInput(values => ({...values, [name]: value}));
+
+
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(input);
+  }
   return (
-    <div>Home</div>
+    <div className='bg-sky-100 '>
+      <div>
+        <h1 className='text-bold '> Enter Product data</h1>
+        <form className='flex-col' onSubmit={handleSubmit}>
+          <label>Product</label>
+        <input type='text'name='product' value={input.product || ""} placeholder='enter your product' onChange={handleChange}/><br></br>
+        <label>Price</label>
+        <input type='number' name="price" value={input.age || ""} placeholder='enter product price' onChange={handleChange}/><br></br>
+        <label>Product Owner</label>
+        <input type='text' name="owner" value={input.owner || ""} placeholder='product ' onChange={handleChange}/>
+        <div>
+          <button  type="submit" className='bg-blue-800' onClick={"submit"}>
+            Submit
+          </button>
+        </div>
+        </form>
+        </div>
+    </div>
   )
 }
 
